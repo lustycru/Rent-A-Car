afaab4ad878816f272dadc63184dafe26791718e
lustycru <jneveu@edenschool.fr>
2025-04-16 15:11:23 +0200

    activate search form on home page

--- rent-car/app/Http/Controllers/HomeController.php
+++ rent-car/app/Http/Controllers/HomeController.php
@@ -16,4 +16,36 @@ class HomeController extends Controller
 
         return view('welcome', ['vehFuel' => $vehFuel, 'vehType' => $vehType, 'vehTrans' => $vehTrans, 'allVeh' => $allVehiclesWithInnerJoinLimit6]);
     }
+
+    public function send(Request $request) {
+        $data = $request->validate(['veh-type' => 'nullable', 'energy-type'  => 'nullable', 'gear-type'  => 'nullable']);
+
+        $vehFuel = DB::select('SELECT DISTINCT fuel_type FROM vehicules');
+        $vehTrans = DB::select('SELECT DISTINCT transmission FROM vehicules');
+        $vehType = DB::select('SELECT DISTINCT name FROM vehicules_types');
+
+        if ($request) {
+            if ($data['veh-type']) {
+                $allVehiclesWithInnerJoin = DB::select("SELECT v.id AS vehId, v.brand AS vehBrand, v.model AS vehModel, v.year AS vehYear, v.price_per_day AS vehPriceDay, v.doors AS vehDoors, v.fuel_type AS vehFuel, v.air_conditioning AS vehAir, v.seats AS vehSeats, v.transmission AS vehTransmission, v.vehicule_type_id AS vehTypeId, vt.id AS vtId, vt.name AS vtName, vp.image_url AS vehPhotos FROM vehicules v INNER JOIN vehicules_types vt ON v.vehicule_type_id = vt.id INNER JOIN vehicules_photos vp ON v.id = vp.vehicule_id WHERE vt.name = '" . strtolower($data['veh-type']) . "';");
+            }
+
+            if ($data['veh-type'] && $data['energy-type']) {
+                $allVehiclesWithInnerJoin = DB::select("SELECT v.id AS vehId, v.brand AS vehBrand, v.model AS vehModel, v.year AS vehYear, v.price_per_day AS vehPriceDay, v.doors AS vehDoors, v.fuel_type AS vehFuel, v.air_conditioning AS vehAir, v.seats AS vehSeats, v.transmission AS vehTransmission, v.vehicule_type_id AS vehTypeId, vt.id AS vtId, vt.name AS vtName, vp.image_url AS vehPhotos FROM vehicules v INNER JOIN vehicules_types vt ON v.vehicule_type_id = vt.id INNER JOIN vehicules_photos vp ON v.id = vp.vehicule_id WHERE vt.name = '" . strtolower($data['veh-type']) . "' AND v.fuel_type = '" . strtolower($data['energy-type']) . "';");
+            }
+
+            if ($data['veh-type'] && $data['gear-type']) {
+                $allVehiclesWithInnerJoin = DB::select("SELECT v.id AS vehId, v.brand AS vehBrand, v.model AS vehModel, v.year AS vehYear, v.price_per_day AS vehPriceDay, v.doors AS vehDoors, v.fuel_type AS vehFuel, v.air_conditioning AS vehAir, v.seats AS vehSeats, v.transmission AS vehTransmission, v.vehicule_type_id AS vehTypeId, vt.id AS vtId, vt.name AS vtName, vp.image_url AS vehPhotos FROM vehicules v INNER JOIN vehicules_types vt ON v.vehicule_type_id = vt.id INNER JOIN vehicules_photos vp ON v.id = vp.vehicule_id WHERE vt.name = '" . strtolower($data['veh-type']) . "' AND v.transmission = '" . strtolower($data['gear-type']) . "';");
+            }
+
+            if ($data['energy-type'] && $data['gear-type']) {
+                $allVehiclesWithInnerJoin = DB::select("SELECT v.id AS vehId, v.brand AS vehBrand, v.model AS vehModel, v.year AS vehYear, v.price_per_day AS vehPriceDay, v.doors AS vehDoors, v.fuel_type AS vehFuel, v.air_conditioning AS vehAir, v.seats AS vehSeats, v.transmission AS vehTransmission, v.vehicule_type_id AS vehTypeId, vt.id AS vtId, vt.name AS vtName, vp.image_url AS vehPhotos FROM vehicules v INNER JOIN vehicules_types vt ON v.vehicule_type_id = vt.id INNER JOIN vehicules_photos vp ON v.id = vp.vehicule_id WHERE v.fuel_type = '" . strtolower($data['energy-type']) . "' AND v.transmission = '" . strtolower($data['gear-type']) . "';");
+            }
+
+            if ($data['veh-type'] && $data['energy-type'] && $data['gear-type']) {
+                $allVehiclesWithInnerJoin = DB::select("SELECT v.id AS vehId, v.brand AS vehBrand, v.model AS vehModel, v.year AS vehYear, v.price_per_day AS vehPriceDay, v.doors AS vehDoors, v.fuel_type AS vehFuel, v.air_conditioning AS vehAir, v.seats AS vehSeats, v.transmission AS vehTransmission, v.vehicule_type_id AS vehTypeId, vt.id AS vtId, vt.name AS vtName, vp.image_url AS vehPhotos FROM vehicules v INNER JOIN vehicules_types vt ON v.vehicule_type_id = vt.id INNER JOIN vehicules_photos vp ON v.id = vp.vehicule_id WHERE v.fuel_type = '" . strtolower($data['energy-type']) . "' AND v.transmission = '" . strtolower($data['gear-type']) . "' AND vt.name = '" . strtolower($data['veh-type']) . "';");
+            }
+        }
+
+        return view('catalog', ['vehFuel' => $vehFuel, 'vehType' => $vehType, 'vehTrans' => $vehTrans, 'allVeh' => $allVehiclesWithInnerJoin]);
+    }
 }
--- rent-car/routes/web.php
+++ rent-car/routes/web.php
@@ -5,16 +5,15 @@ use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\CatalogController;
 use \App\Http\Controllers\DetailController;
 use \App\Http\Controllers\ReservationController;
-use Illuminate\Support\Facades\Mail;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home.home');
-Route::post('/', [HomeController::class, 'send'])->name('home.form');
+Route::post('/', [HomeController::class, 'send'])->name('home.send');
 
 Route::get('/vehicles', [CatalogController::class, 'index'])->name('vehicles.all');
 Route::get('/vehicle/{id}', [DetailController::class, 'index'])->name('vehicle.detail');;
+
 Route::get('/reservation/{id}', [ReservationController::class, 'index'])->name('reservation.show');
 Route::post('/reservation/{id}', [ReservationController::class, 'send'])->name('reservation.send');
+
 Route::get('/vehicles/{param}/{value}', [CatalogController::class, 'filter']);
-
-
